7dc9d73c7bc51f7e3fc03663b39810ac2e2b5584
Gmzalexx <160627346+Gmzalexx@users.noreply.github.com>
2024-05-31 19:26:31 -0600

    Pruebauno.xaml.cs

--- Complementario01/Pruebauno.xaml.cs
+++ Complementario01/Pruebauno.xaml.cs
@@ -9,17 +9,19 @@ public partial class Pruebauno : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        //Obtenemos los numeros registrados por los usuarios, esto se logra por los entry
+        //Obtenemos los numeros registrados que hemos agregado esto funciona ya que entry tienen una
+        //conexion por los entry1 entry2 entry3 emtry4 estos los declaramos 
+        //En la xaml de prueba uno 
         int num1 = Convert.ToInt32(entry1.Text);
         int num2 = Convert.ToInt32(entry2.Text);
         int num3 = Convert.ToInt32(entry3.Text);
         int num4 = Convert.ToInt32(entry4.Text);
-
-        // Invertimos los números
+	
+        //Con array.Reverse invertiremos los numeros que hemos colocado en los entry
         int[] numeros = { num1, num2, num3, num4 };
         Array.Reverse(numeros);
-
-        // Mostramos los numeros invertidos en el label
+	
+        //Luego mostramos los numeros invertidos en el label este aparecera en la pantalla cuando invertamos los nuemeros
         lblResultado.Text = string.Join(", ", numeros);
     }
 }
